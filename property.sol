cb9639438bec328982f8168c4d40f77bd2aaf39e
Sriyush <108729564+Sriyush@users.noreply.github.com>
2022-11-07 13:13:12 +0530

    Create property.sol

---
+++ property.sol
@@ -0,0 +1,71 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+import "./PriceConverter.sol";
+error notOwner();
+contract property{
+    address[] public seller;
+    address public buyers;
+    uint256 public initialamount;
+    uint256 public price;
+    string need;
+    string r="rent";
+    string b="Buy";
+    mapping (address => uint256) public sells;
+    constructor(){
+        seller=msg.sender;
+    }
+    modifier OnlyOwner{
+        if(msg.sender !=owner){
+            revert notOwner;
+        }
+        _;
+    }
+    function sell() public OnlyOwner{
+        require(msg.sender == owner);
+    }
+    function add() public {
+    seller.push(msg.sender);
+    }
+
+  function getTheLastUser() external view returns(address) {
+    uint lastIndex = seller.length-1;
+    return seller[lastIndex];
+  }
+
+  function getLengthArray() external view returns(uint) {
+    return seller.length;
+  }
+
+    function propertybuy() OnlyBuyer{
+        if(msg.value>=price){
+            address public currentowner = getTheLastUser();
+        }
+        else{
+            propertyNegotiate();
+        }
+    }
+    function Sub() view public returns (uint256)
+    {
+        uint256 ans = price- initialamount;
+        return ans;
+    }
+    function propertyNegotiate() public payable{
+        if(Sub()<=5000);{
+            currentowner =getTheLastUser();
+        }
+        else{
+
+        }
+    }
+    function propertysell() onlyseller{
+        if(keccak256(abi.encodePacked(need)) == keccak256(abi.encodePacked(r))){
+            propertyrent();
+        }
+        else{
+            propertybuy();
+        }
+    }
+    function propertyrent() OnlyOwner{
+        
+    }
+}
